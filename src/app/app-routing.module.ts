ea3d428fa71c0f37ff55c4435210db9221dc2f8d
MOHD MUHSIN <mbeth605@gmail.com>
2023-09-13 11:56:07 +0530

    removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path:'**',component:ErrorPageComponent }
+  { path:'**',component:HomePageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
@@ -11,7 +10,6 @@ import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorPageComponent,
     HomePageComponent,
   ],
   imports: [
