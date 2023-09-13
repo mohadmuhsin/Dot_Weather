ee529b1f937ddc747113b266a98347c0b1970d48
MOHD MUHSIN <mbeth605@gmail.com>
2023-09-13 13:07:26 +0530

    some changes

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -2,6 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MainServiceService } from 'src/app/service/main-service.service';
 import { Environment } from 'src/app/environments/environment';
+import { weatherIcon } from 'src/app/model/weather';
+//  export enum images{
+//    sunny ='',
+//    snow = ''
+//   haze:
+//   clear:
+//   rainy:
+//   clouds:
+//   drizziling:
+//   }
 
 @Component({
   selector: 'app-home-page',
@@ -12,28 +22,24 @@ export class HomePageComponent implements OnInit {
   code: any
   text: string = ''
   api_key: string = ''
-  
+ 
+  img!:string
   wind!: number 
   temp!:number
   location:string=''
   humidity!: number
   invalid!: boolean
-  
-  // weathers
-  sunny:boolean = false
-  snow: boolean = false
-  haze: boolean = false
-  clear: boolean = false
-  rainy: boolean = false
-  clouds: boolean = false
-  drizziling: boolean = false
+  weatherIcon:any[]=weatherIcon
   error!: string;
   
   constructor(
     private http: HttpClient,
-    private service:MainServiceService
+    private service: MainServiceService,
+    
   ) {
     this.api_key = Environment.api_key
+
+
   }
 
   ngOnInit(): void {
@@ -51,34 +57,20 @@ export class HomePageComponent implements OnInit {
     this.service.getData(this.text).subscribe({
       next: (res: any) =>
       {
-        this.sunny = false
-        this.snow = false
-        this.haze = false
-        this.clear = false
-        this.rainy = false
-        this.clouds = false
-        this.drizziling = false
         this.location = res.name
         this.temp = res.main.temp
         this.wind = res.wind.speed
         this.code = res.weather[0].icon
         this.humidity = res.main.humidity
-    
-        if (this.code==="01d" || this.code === "01n") {
-           this.clear = true
-        } else if (this.code === "02d" || this.code === "02n") {
-          this.clouds =true
-        }else if (this.code === "03d" || this.code === "03n") {
-          this.drizziling =true
-        }else if (this.code === "04d" || this.code === "04n") {
-          this.drizziling =true
-        }else if (this.code === "09d" || this.code === "09n") {
-          this.rainy =true
-        }else if (this.code === "010d" || this.code === "010n") {
-          this.rainy =true
-        }else if (this.code === "013d" || this.code === "013n") {
-          this.snow =true
-        }
+
+        let items: any = this.weatherIcon.find((items: any) => {
+          let findings = items.code.find((code: any) =>{ console.log(code);
+            
+            if(code == this.code) return code
+          })
+          return findings
+        })
+        this.img=items.img
       },
       error: (err: any) =>
       {
---
+++ src/app/model/weather.ts
@@ -0,0 +1,60 @@
+// export const weatherIcon  = [{
+//     img:'assets/cloud.png',code:'01d',
+    
+//   },{
+//     img:'assets/cloud.png',code:'01n',
+    
+//   },{
+//     img:'assets/cloud.png',code:'02d',
+    
+//   },{
+//     img:'assets/cloud.png',code:'02n',
+    
+//   },{
+//     img:'assets/drizzle.png',code:'03d',
+    
+//   },{
+//     img:'assets/drizzle.png',code:'03n',
+    
+//   },{
+//     img:'assets/snow.png',code:'04d',
+    
+//   },{
+//     img:'assets/snow.png',code:'04n',
+    
+//   },{
+//     img:'assets/rain.png',code:'09d',
+    
+//   },{
+//     img:'assets/rain.png',code:'09n',
+    
+//   },{
+//     img:'assets/rain.png',code:'010d',
+    
+//   },{
+//     img:'assets/rain.png',code:'010n',
+    
+//   },{
+//     img:'assets/snow.png',code:'013d',
+    
+//   },{
+//     img:'assets/snow.png',code:'013n',
+    
+//   }]
+
+export const weatherIcon  = [{
+    img:'assets/cloud.png',code:['01d','01n','02d','02n'],
+    
+  },{
+    img:'assets/drizzle.png',code:['03d','03n',],
+    
+  },{
+    img:'assets/snow.png',code:['04d','04n'],
+    
+  },{
+    img:'assets/rain.png',code:['09d','09n','010d','010n',],
+    
+  },{
+    img:'assets/snow.png',code:['013d','013n'],
+    
+  }]
