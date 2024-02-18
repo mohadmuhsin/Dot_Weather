e1edc498e042a3ad3edf550dfcbd49c60fadb31e
Mohammed Muhsin v v <mbeth605@gmail.com>
2024-02-18 08:24:16 +0530

    refactor(buildspec)-added buildspec

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -5,87 +5,76 @@ import { Environment } from 'src/app/environments/environment';
 import { weatherIcon } from 'src/app/model/weather';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 
-
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
-  code: any
-  text: string = ''
-  api_key: string = ''
- 
-  img!:string
-  wind!: number 
-  temp!:number
-  location:string=''
-  humidity!: number
-  invalid!: boolean
-  weatherIcon:any[]= weatherIcon
+  code: any;
+  text: string = '';
+  api_key: string = '';
+
+  img!: string;
+  wind!: number;
+  temp!: number;
+  location: string = '';
+  humidity!: number;
+  invalid!: boolean;
+  weatherIcon: any[] = weatherIcon;
   error!: string;
-  
-  constructor(
-    private http: HttpClient,
-    private service: MainServiceService,
-    
-  ) {
-    this.api_key = Environment.api_key
-
-
+  font: string = '';
+  constructor(private http: HttpClient, private service: MainServiceService) {
+    this.api_key = Environment.api_key;
   }
 
   ngOnInit(): void {
-    this.search()
-  
+    this.search();
   }
 
   debouncedSearch = this.debounce(() => {
-    this.invalid = false
+    this.invalid = false;
     this.search();
   }, 1000);
 
   search() {
-
-    this.service.getData(this.text)
-    //   .pipe(
-    //   debounceTime(1000),
-    //   distinctUntilChanged()
-    // )
+    this.service
+      .getData(this.text)
+      //   .pipe(
+      //   debounceTime(1000),
+      //   distinctUntilChanged()
+      // )
       .subscribe({
-      next: (res: any) =>
-      {
-        this.location = res.name
-        this.temp = res.main.temp
-        this.wind = res.wind.speed
-        this.code = res.weather[0].icon
-        this.humidity = res.main.humidity
-        
-        let items: any = this.weatherIcon.find((item: any) => {
-          return item.code.includes(this.code);
-        });
-          
-        this.img=items.img
-      },
-      error: (err: any) =>
-      {
-        if (err.status === 404) {
-          this.invalid = true
-          this.error  = 'No Matched City Found!!'
-        }
-      }
-    })
+        next: (res: any) => {
+          this.location = res.name;
+          this.temp = res.main.temp;
+          this.wind = res.wind.speed;
+          this.code = res.weather[0].icon;
+          this.humidity = res.main.humidity;
+
+          let items: any = this.weatherIcon.find((item: any) => {
+            return item.code.includes(this.code);
+          });
+
+          this.img = items.img;
+        },
+        error: (err: any) => {
+          if (err.status === 404) {
+            this.invalid = true;
+            this.error = 'No Matched City Found!!';
+          }
+        },
+      });
   }
 
   // debounce method
   debounce(func: Function, delay: number) {
     let timeoutId: any;
-    return  (...args: any) => {
+    return (...args: any) => {
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => {
         func.apply(this, args);
       }, delay);
     };
   }
-
 }
